0537f73431cf1d4d4cb7b5be3fb6ae9f15961cf4
rachidj <rachid.jeffali@gmail.com>
2021-03-19 10:43:19 +0100

    modifications main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+PI = 3.14
+
 def calculAir(rayon):
-    return 3.14 * rayon * rayon
+    return 3.14 * rayon ^ 2
 
 def calculVolume(rayon):
-    return 4 * rayon * rayon * rayon / 3
+    return 4 * rayon ^ 3 / 3
+
+def calculPerimetre(r):
+    print("Calcul du perimetre")
+    return 2 * PI * r
